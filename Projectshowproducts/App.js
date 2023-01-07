1ac189a5b88d3651f6a02a6d264f4a0c0026752a
Chirag Thakkar <cthakkar853@gmail.com>
2023-01-07 11:25:20 +0530

    update

---
+++ Projectshowproducts/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Footer from './component/Footer';
+import Header from './component/Header';
+import Index from './pages/Index';
+import About from './pages/About';
+import Products from './pages/Products';
+import Fashion from './pages/Fashion';
+import News from './pages/News';
+import Contact from './pages/Contact';
+import Product_Details from './pages/Product_Details';
+
+function App() {
+  return (
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<><Header/><Index/><Footer/></>}></Route>
+        <Route path='/index' element={<><Header/><Index/><Footer/></>}></Route>
+        <Route path='/about' element={<><Header/><About/> <Footer/></>}></Route>
+        <Route path='/products' element={<><Header/><Products/><Footer/></>}></Route>
+        <Route path='/fashion' element={<><Header/><Fashion/><Footer/></>}></Route>
+        <Route path='/news' element={<><Header/><News/><Footer/></>}></Route>
+        <Route path='/contact' element={<><Header/><Contact/><Footer/></>}></Route>
+        <Route path='/viewdetails/:id' element={<><Header/><Product_Details/><Footer/></>}></Route>
+      </Routes>
+    </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
